469bf9f7d61cc5ceaf94d58243fe245affc15a03
melylugo <88989879+melylugo@users.noreply.github.com>
2021-09-06 18:32:11 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -2,6 +2,14 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    
+    num=int(input("Escribe un numero : "))
+    acum=1
+
+    while (acum**2)<=num:
+        acum=acum+1
+    print (acum)
+
     pass
 
 if __name__=='__main__':
