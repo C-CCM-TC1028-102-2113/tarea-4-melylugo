15a89eec45178e46c5b2b19c9b90934c0ae3082b
melylugo <88989879+melylugo@users.noreply.github.com>
2021-09-06 16:08:06 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,5 +1,22 @@
 def main():
     #escribe tu código abajo de esta línea
+    valor=int(input())
+    cont=0
+    while valor%2!=0:
+        print("1"+"-#")
+        for valor in range (1,valor,2):
+            cont=cont+2
+            valor=valor+1
+            print (str(valor)+"-%")
+            while cont>=valor:
+                valor=valor+1
+                print (str(valor)+"-#")
+        break
+    else:
+        for valor in range (1,valor,2):
+            print (str(valor)+"-#")
+            valor=valor+1
+            print (str(valor)+"-%")
     pass
 
 if __name__=='__main__':   
