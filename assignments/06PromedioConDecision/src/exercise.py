47124f0468685ef1bedec51901f246dd24f199e7
melylugo <88989879+melylugo@users.noreply.github.com>
2021-09-07 12:46:56 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -2,16 +2,15 @@ def main():
     #escribe tu código abajo de esta línea
     cont=0
     sum=0
-
-    num=float(input()) 
-    while num>=0: 
-        sum=sum+num
+    
+    n=float(input())
+    while n>=0:
+        sum=sum+n
         cont=cont+1
-        num=float(input()) 
-        if num<0:
-            break 
-    promedio=sum/cont
-    print (promedio)
+        n=float(input())
+    prom=sum/cont
+    print(prom)
+
     pass
 if __name__=='__main__':
     main()
